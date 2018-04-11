f6119f2a1f99836fe66630c23130d67f85e4b00b
KALTURA\Shmulik.Kummer <shmulik.kummer@kaltura.com>
2018-04-11 17:25:48 +0300

    Updated  AddTests.java

--- src/test/java/com/kaltura/client/test/tests/servicesTests/bookmarkTests/AddTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/bookmarkTests/AddTests.java
@@ -4,11 +4,15 @@ import com.kaltura.client.enums.*;
 import com.kaltura.client.test.servicesImpl.AssetServiceImpl;
 import com.kaltura.client.test.servicesImpl.BookmarkServiceImpl;
 import com.kaltura.client.test.tests.BaseTest;
+import com.kaltura.client.test.utils.BookmarkUtils;
 import com.kaltura.client.types.*;
 import com.kaltura.client.utils.response.base.Response;
 import io.qameta.allure.Description;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,6 +24,7 @@ public class AddTests extends BaseTest {
     private int fileId;
     private BookmarkActionType actionType;
     private int position = 0;
+    private List<String> assetList = new ArrayList<>();
     // instantiate Bookmark object
     private Bookmark bookmark = new Bookmark();
     // instantiate BookmarkPlayerData object
@@ -30,32 +35,25 @@ public class AddTests extends BaseTest {
     @BeforeClass
     private void add_tests_before_class() {
         // Get VOD asset id from ingest
+
         assetId = mediaAsset.getId();
+
         type = AssetType.get(AssetType.MEDIA.getValue());
         AssetReferenceType assetReferenceType = AssetReferenceType.get(AssetReferenceType.MEDIA.getValue());
-        Response<Asset> assetResponse = AssetServiceImpl.get(sharedMasterUserKs,String.valueOf(assetId),assetReferenceType);
+        Response<Asset> assetResponse = AssetServiceImpl.get(sharedMasterUserKs, String.valueOf(assetId), assetReferenceType);
         fileId = assetResponse.results.getMediaFiles().get(0).getId();
         actionType = BookmarkActionType.get(BookmarkActionType.FIRST_PLAY.getValue());
 
+        assetList.add(String.valueOf(assetId));
         // Initialize bookmark object parameters
-        bookmark.setType(type);
-        bookmark.setId(String.valueOf(assetId));
-        bookmark.setPosition(position);
+        bookmark = BookmarkUtils.addBookmark(0, String.valueOf(assetId), fileId, AssetType.MEDIA, BookmarkActionType.FIRST_PLAY);
 
-        // Initialize playerData object parameters
-        playerData.setAction(actionType);
-        playerData.setAverageBitrate(0);
-        playerData.setTotalBitrate(0);
-        playerData.setCurrentBitrate(0);
-        playerData.setFileId((long) fileId);
-        bookmark.setPlayerData(playerData);
+        // Initialize bookmarkFilter object parameters
+        bookmarkFilter = BookmarkUtils.listBookmark(BookmarkOrderBy.POSITION_ASC, AssetType.MEDIA, assetList);
 
         // Initialize asset id
         bookmarkFilter.setAssetIdIn(String.valueOf(assetId));
-        // Initialize orderBy
-        bookmarkFilter.setOrderBy(BookmarkOrderBy.POSITION_DESC.getValue());
-        // Initialize asset type
-        bookmarkFilter.setAssetTypeEqual(type);
+
     }
 
     @Description("bookmark/action/add - first play")
@@ -71,14 +69,14 @@ public class AddTests extends BaseTest {
 
 
         // Invoke bookmark/action/list to verify insertion of bookmark position
-        Response<ListResponse<Bookmark>> bookmarkListResponse = BookmarkServiceImpl.list(sharedMasterUserKs,bookmarkFilter);
+        Response<ListResponse<Bookmark>> bookmarkListResponse = BookmarkServiceImpl.list(sharedMasterUserKs, bookmarkFilter);
         Bookmark bookmark1 = bookmarkListResponse.results.getObjects().get(0);
 
         // Assertions
         // ***********************************************
 
         // Match content of asset id
-        assertThat( bookmark1.getId()).isEqualTo(String.valueOf(assetId));
+        assertThat(bookmark1.getId()).isEqualTo(String.valueOf(assetId));
 
         // Match content of asset position
         assertThat(bookmark1.getPosition()).isEqualTo(this.position);
@@ -97,11 +95,11 @@ public class AddTests extends BaseTest {
 
     }
 
-    @Description("bookmark/action/add - stop")
+    @Description("bookmark/action/add - pause")
     @Test
     private void stopPlayback() {
-        // Set action type to "STOP"
-        actionType = BookmarkActionType.get(BookmarkActionType.STOP.getValue());
+        // Set action type to "PAUSE"
+        actionType = BookmarkActionType.get(BookmarkActionType.PAUSE.getValue());
         playerData.setAction(actionType);
         position = 30;
         bookmark.setPosition(position);
@@ -114,7 +112,7 @@ public class AddTests extends BaseTest {
         assertThat(booleanResponse.error).isNull();
 
         // Invoke bookmark/action/list to verify insertion of bookmark position
-        Response<ListResponse<Bookmark>> bookmarkListResponse2 = BookmarkServiceImpl.list(sharedMasterUserKs,bookmarkFilter);
+        Response<ListResponse<Bookmark>> bookmarkListResponse2 = BookmarkServiceImpl.list(sharedMasterUserKs, bookmarkFilter);
         Bookmark bookmark2 = bookmarkListResponse2.results.getObjects().get(0);
 
         // Match content of asset position
@@ -124,6 +122,8 @@ public class AddTests extends BaseTest {
     @Description("bookmark/action/add - 95% watching == finish watching")
     @Test
     private void watchingNinetyFive() {
+        actionType = BookmarkActionType.get(BookmarkActionType.PLAY.getValue());
+        playerData.setAction(actionType);
         position = 999;
         bookmark.setPosition(position);
 
@@ -135,7 +135,7 @@ public class AddTests extends BaseTest {
         assertThat(booleanResponse.error).isNull();
 
         // Invoke bookmark/action/list to verify insertion of bookmark position
-        Response<ListResponse<Bookmark>> bookmarkListResponse3 = BookmarkServiceImpl.list(sharedMasterUserKs,bookmarkFilter);
+        Response<ListResponse<Bookmark>> bookmarkListResponse3 = BookmarkServiceImpl.list(sharedMasterUserKs, bookmarkFilter);
         Bookmark bookmark3 = bookmarkListResponse3.results.getObjects().get(0);
 
         // Assertions
@@ -146,9 +146,25 @@ public class AddTests extends BaseTest {
 
     }
 
+    @Description("bookmark/action/add - back to start - position:0")
+    @Test
+    private void backToStart() {
+        actionType = BookmarkActionType.get(BookmarkActionType.STOP.getValue());
+        playerData.setAction(actionType);
+        position = 0;
+        bookmark.setPosition(position);
+        Response<Boolean> booleanResponse = BookmarkServiceImpl.add(sharedMasterUserKs, bookmark);
+
+        Response<ListResponse<Bookmark>> bookmarkListResponse4 = BookmarkServiceImpl.list(sharedMasterUserKs, bookmarkFilter);
+        Bookmark bookmark4 = bookmarkListResponse4.results.getObjects().get(0);
+        // Verify finishedWatching = false
+        assertThat(bookmark4.getFinishedWatching()).isFalse();
+    }
+
+
     @Description("bookmark/action/add - finish watching")
     @Test
-    private void FinishWatching() {
+    private void finishWatching() {
         // Set action type to "FINISH"
         actionType = BookmarkActionType.get(BookmarkActionType.FINISH.getValue());
         playerData.setAction(actionType);
@@ -163,14 +179,14 @@ public class AddTests extends BaseTest {
         assertThat(booleanResponse.error).isNull();
 
         // Invoke bookmark/action/list to verify insertion of bookmark position
-        Response<ListResponse<Bookmark>> bookmarkListResponse4 = BookmarkServiceImpl.list(sharedMasterUserKs,bookmarkFilter);
-        Bookmark bookmark4 = bookmarkListResponse4.results.getObjects().get(0);
+        Response<ListResponse<Bookmark>> bookmarkListResponse5 = BookmarkServiceImpl.list(sharedMasterUserKs, bookmarkFilter);
+        Bookmark bookmark5 = bookmarkListResponse5.results.getObjects().get(0);
 
         // Assertions
         // ***********************************************
 
         // Verify finishedWatching = true
-        assertThat(bookmark4.getFinishedWatching()).isTrue();
+        assertThat(bookmark5.getFinishedWatching()).isTrue();
 
     }
 
