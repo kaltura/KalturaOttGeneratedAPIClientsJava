0a6a327471620d005886c07e88d639ddee3d3661
alon.basin <alon.basin@kaltura.com>
2018-04-18 15:15:15 +0300

    service impl

--- src/test/java/com/kaltura/client/test/servicesImpl/LanguageServiceImpl.java
+++ src/test/java/com/kaltura/client/test/servicesImpl/LanguageServiceImpl.java
@@ -1,32 +1,37 @@
 package com.kaltura.client.test.servicesImpl;
 
-import com.kaltura.client.types.InboxMessage;
+import com.kaltura.client.Client;
+import com.kaltura.client.services.LanguageService;
+import com.kaltura.client.test.TestAPIOkRequestsExecutor;
+import com.kaltura.client.types.Language;
+import com.kaltura.client.types.LanguageFilter;
 import com.kaltura.client.types.ListResponse;
+import com.kaltura.client.utils.response.base.ApiCompletion;
 import com.kaltura.client.utils.response.base.Response;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.kaltura.client.services.LanguageService.*;
+import static org.awaitility.Awaitility.await;
 
 public class LanguageServiceImpl {
 
     private static final AtomicBoolean done = new AtomicBoolean(false);
 
-    private static Response<InboxMessage> inboxMessageResponse;
-    private static Response<ListResponse<InboxMessage>> inboxMessageListResponse;
-    private static Response<Boolean> booleanResponse;
+    private static Response<ListResponse<Language>> languageListResponse;
 
-//    // list
-//    public static void list(Client client) {
-//
-//        InboxMessageService.GetInboxMessageBuilder getInboxMessageBuilder = InboxMessageService.get(id)
-//                .setCompletion((ApiCompletion<InboxMessage>) result -> {
-//                    inboxMessageResponse = result;
-//                    done.set(true);
-//                });
-//
-//        TestAPIOkRequestsExecutor.getExecutor().queue(getInboxMessageBuilder.build(client));
-//        await().untilTrue(done);
-//        done.set(false);
-//
-//        return inboxMessageResponse;
-//    }
+    // list
+    public static Response<ListResponse<Language>> list(Client client, LanguageFilter languageFilter) {
+        ListLanguageBuilder listLanguageBuilder = LanguageService.list(languageFilter)
+                .setCompletion((ApiCompletion<ListResponse<Language>>) result -> {
+                    languageListResponse = result;
+                    done.set(true);
+                });
+
+        TestAPIOkRequestsExecutor.getExecutor().queue(listLanguageBuilder.build(client));
+        await().untilTrue(done);
+        done.set(false);
+
+        return languageListResponse;
+    }
 }
