18f09a3f365d126589cb302929387fa649923f21
alon.basin <alon.basin@kaltura.com>
2018-04-22 16:35:12 +0300

    no message

--- src/test/java/com/kaltura/client/test/servicesImpl/BaseServiceImpl.java
+++ src/test/java/com/kaltura/client/test/servicesImpl/BaseServiceImpl.java
@@ -3,7 +3,6 @@ package com.kaltura.client.test.servicesImpl;
 import com.kaltura.client.Client;
 import com.kaltura.client.test.TestAPIOkRequestsExecutor;
 import com.kaltura.client.utils.request.RequestBuilder;
-import com.kaltura.client.utils.response.base.Response;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -13,11 +12,9 @@ public class BaseServiceImpl {
 
     protected static final AtomicBoolean done = new AtomicBoolean(false);
 
-    public static Response executeRequest(Client client, RequestBuilder requestBuilder, Response response) {
+    static void executeRequest(Client client, RequestBuilder requestBuilder) {
         TestAPIOkRequestsExecutor.getExecutor().queue(requestBuilder.build(client));
         await().untilTrue(done);
         done.set(false);
-
-        return response;
     }
 }
