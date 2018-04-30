ce333c02d84ae3c58259ced809a038247eaa58d9
KALTURA\Shmulik.Kummer <shmulik.kummer@kaltura.com>
2018-04-30 17:23:08 +0300

    Added new class - ChannelAddTests.java

---
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/channelTests/ChannelAddTests.java
@@ -0,0 +1,25 @@
+package com.kaltura.client.test.tests.servicesTests.channelTests;
+
+import com.kaltura.client.Client;
+import com.kaltura.client.test.tests.BaseTest;
+import io.qameta.allure.Description;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ChannelAddTests extends BaseTest {
+
+    private Client client;
+
+    @BeforeClass
+    private void get_tests_before_class() {
+
+    }
+
+    @Description ("channel/action/add")
+    @Test
+
+    private void addChannel() {
+
+    }
+
+}
