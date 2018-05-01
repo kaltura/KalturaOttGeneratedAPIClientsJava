22c1a4a3190f5fd01f41534f2266021bb331e593
KALTURA\Shmulik.Kummer <shmulik.kummer@kaltura.com>
2018-05-01 13:59:18 +0300

    Added new util to AssetUtils

--- src/test/java/com/kaltura/client/test/utils/AssetUtils.java
+++ src/test/java/com/kaltura/client/test/utils/AssetUtils.java
@@ -3,10 +3,10 @@ package com.kaltura.client.test.utils;
 import com.kaltura.client.Client;
 import com.kaltura.client.enums.AssetReferenceType;
 import com.kaltura.client.test.servicesImpl.AssetServiceImpl;
-import com.kaltura.client.types.Asset;
-import com.kaltura.client.types.MediaFile;
+import com.kaltura.client.types.*;
 import com.kaltura.client.utils.response.base.Response;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +14,21 @@ import static com.kaltura.client.test.tests.BaseTest.SharedHousehold.getSharedMa
 import static com.kaltura.client.test.tests.BaseTest.getClient;
 
 public class AssetUtils extends BaseUtils {
+
+
+    public static SearchAssetFilter getSearchAssetFilter(@Nullable String ksql, @Nullable String idIn, @Nullable String typeIn,
+                                                      @Nullable DynamicOrderBy dynamicOrderBy, List<AssetGroupBy> groupBy, String name, String orderBy) {
+        SearchAssetFilter searchAssetFilter = new SearchAssetFilter();
+        searchAssetFilter.setKSql(ksql);
+        searchAssetFilter.setIdIn(idIn);
+        searchAssetFilter.setTypeIn(typeIn);
+        searchAssetFilter.setDynamicOrderBy(dynamicOrderBy);
+        searchAssetFilter.setGroupBy(groupBy);
+        searchAssetFilter.setName(name);
+        searchAssetFilter.setOrderBy(orderBy);
+
+        return searchAssetFilter;
+    }
 
     public static List<Integer> getAssetFileIds(String assetId) {
         Client client = getClient(getSharedMasterUserKs());
