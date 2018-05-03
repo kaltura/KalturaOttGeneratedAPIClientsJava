745bbf8da375bf138162e84ad2f0aeacc0a45209
alon.basin <alon.basin@kaltura.com>
2018-05-03 12:13:22 +0300

    refacor ottUser listTests

--- src/test/java/com/kaltura/client/test/tests/servicesTests/ottUserTests/UpdateTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/ottUserTests/UpdateTests.java
@@ -1,7 +1,6 @@
 package com.kaltura.client.test.tests.servicesTests.ottUserTests;
 
 import com.kaltura.client.Client;
-import com.kaltura.client.test.servicesImpl.OttUserServiceImpl;
 import com.kaltura.client.test.tests.BaseTest;
 import com.kaltura.client.types.LoginResponse;
 import com.kaltura.client.types.OTTUser;
@@ -12,7 +11,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static com.kaltura.client.test.servicesImpl.OttUserServiceImpl.register;
 import static com.kaltura.client.test.utils.OttUserUtils.generateOttUser;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +26,7 @@ public class UpdateTests extends BaseTest {
     @BeforeClass
     private void ottUser_update_tests_setup() {
         client = getClient(null);
+        // register user
         ottUserResponse = register(client, partnerId, generateOttUser(), defaultUserPassword);
         user = ottUserResponse.results;
         originalUserEmail = user.getEmail();
