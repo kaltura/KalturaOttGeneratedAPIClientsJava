d3248ee7ed2276dc4fc01af5d6efcf4d30f4aa7a
Shmulik Kummer <kummersh@gmail.com>
2018-05-31 10:16:44 +0300

    Updated class - "listVodAssetsWithLikeKsqlQuery"

--- src/test/java/com/kaltura/client/test/tests/servicesTests/AssetTests/AssetListTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/AssetTests/AssetListTests.java
@@ -3,6 +3,7 @@ package com.kaltura.client.test.tests.servicesTests.AssetTests;
 import com.kaltura.client.services.AssetService;
 import com.kaltura.client.test.tests.BaseTest;
 import com.kaltura.client.test.utils.AssetUtils;
+import com.kaltura.client.test.utils.BaseUtils;
 import com.kaltura.client.test.utils.IngestUtils;
 import com.kaltura.client.types.Asset;
 import com.kaltura.client.types.AssetFilter;
@@ -12,6 +13,7 @@ import com.kaltura.client.utils.response.base.Response;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.assertj.core.api.Assertions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -20,10 +22,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.kaltura.client.test.IngestConstants.MOVIE_MEDIA_TYPE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static com.kaltura.client.services.AssetService.*;
+
+import org.hamcrest.collection.IsEmptyCollection;
+
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.collect;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+
+import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AssetListTests extends BaseTest {
 
@@ -34,9 +50,10 @@ public class AssetListTests extends BaseTest {
     private AssetFilter assetFilter = null;
     private ListAssetBuilder listAssetBuilder = null;
     private Map map = new HashMap();
+    private ArrayList<Long> assetList = new ArrayList<>();
     private ArrayList<String> list = new ArrayList<>();
     private String tagName = "Genre";
-    private String tagValue = "shmulik_1233";
+    private String tagValue = BaseUtils.getRandomValue(tagName +"_",999999);
 
     @BeforeClass
     private void Asset_list_before_class() {
@@ -48,6 +65,8 @@ public class AssetListTests extends BaseTest {
         asset2 = IngestUtils.ingestVOD(MOVIE_MEDIA_TYPE, map);
         asset3 = IngestUtils.ingestVOD(MOVIE_MEDIA_TYPE, map);
 
+        assetList.add(asset2.getId());
+        assetList.add(asset3.getId());
     }
 
     // KalturaSearchAssetFilter
@@ -115,28 +134,38 @@ public class AssetListTests extends BaseTest {
     }
 
 
-    // TODO - complete test after ingest util was updated
     @Severity(SeverityLevel.CRITICAL)
     @Description("Asset/action/list - VOD - not query")
     @Test
     private void listVodAssetsWithNotKsqlQuery() {
-
-
-    }
-
-    // TODO - complete test after ingest util was updated
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Asset/action/list - VOD - like query")
-    @Test
-    private void listVodAssetsWithLikeKsqlQuery() {
-        ksqlQuery = "name ~ '" + asset.getName() + "'";
+        ksqlQuery = "(and media_id != '" + asset3.getId() + "' " + tagName + " = '" + tagValue + "')";
+        System.out.println(ksqlQuery + "1234");
         assetFilter = AssetUtils.getSearchAssetFilter(ksqlQuery, null, null, null, null,
                 null, null);
         listAssetBuilder = AssetService.list(assetFilter, null)
                 .setKs(BaseTest.SharedHousehold.getSharedMasterUserKs());
+
         Response<ListResponse<Asset>> assetListResponse = executor.executeSync(listAssetBuilder);
         assertThat(assetListResponse.results.getTotalCount()).isEqualTo(1);
-        assertThat(assetListResponse.results.getObjects().get(0).getId()).isEqualTo(asset.getId());
+        assertThat(assetListResponse.results.getObjects().get(0).getId()).isEqualTo(asset2.getId());
+
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Asset/action/list - VOD - like query")
+    @Test
+    private void listVodAssetsWithLikeKsqlQuery() {
+        ksqlQuery = "" + tagName + " ~ '" + tagValue + "'";
+        assetFilter = AssetUtils.getSearchAssetFilter(ksqlQuery, null, null, null, null,
+                null, null);
+        listAssetBuilder = AssetService.list(assetFilter, null)
+                .setKs(BaseTest.SharedHousehold.getSharedMasterUserKs());
+        List<Asset> assets = executor.executeSync(listAssetBuilder).results.getObjects();
+        assertThat(assets.size()).isEqualTo(2);
+
+        List<Long> assetIds = assets.stream().map(Asset::getId).collect(Collectors.toList());
+        assertThat(assetIds).contains(asset3.getId(),asset2.getId());
+        assertThat(assetIds).doesNotContain(asset.getId());
     }
 
     // TODO - complete test after ingest util was updated
