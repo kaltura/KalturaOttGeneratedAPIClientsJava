4ab32f7e0bd4327188c679ecbbd8bb0d2c42b5d0
Shmulik Kummer <kummersh@gmail.com>
2018-05-31 11:35:49 +0300

    Updated AssetListTests class

--- src/test/java/com/kaltura/client/test/tests/servicesTests/AssetTests/AssetListTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/AssetTests/AssetListTests.java
@@ -5,15 +5,11 @@ import com.kaltura.client.test.tests.BaseTest;
 import com.kaltura.client.test.utils.AssetUtils;
 import com.kaltura.client.test.utils.BaseUtils;
 import com.kaltura.client.test.utils.IngestUtils;
-import com.kaltura.client.types.Asset;
-import com.kaltura.client.types.AssetFilter;
-import com.kaltura.client.types.ListResponse;
-import com.kaltura.client.types.MediaAsset;
+import com.kaltura.client.types.*;
 import com.kaltura.client.utils.response.base.Response;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import org.assertj.core.api.Assertions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,24 +18,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static com.kaltura.client.test.IngestConstants.MOVIE_MEDIA_TYPE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static com.kaltura.client.services.AssetService.*;
-
-import org.hamcrest.collection.IsEmptyCollection;
-
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.collect;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
-import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
-
-import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
-import static org.hamcrest.number.OrderingComparison.lessThan;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AssetListTests extends BaseTest {
 
@@ -59,7 +41,6 @@ public class AssetListTests extends BaseTest {
     private void Asset_list_before_class() {
         // Get asset from shared asset method
         asset = BaseTest.getSharedMediaAsset();
-        // Ingest asset2
         list.add(tagValue);
         map.put(tagName, list);
         asset2 = IngestUtils.ingestVOD(MOVIE_MEDIA_TYPE, map);
@@ -139,7 +120,6 @@ public class AssetListTests extends BaseTest {
     @Test
     private void listVodAssetsWithNotKsqlQuery() {
         ksqlQuery = "(and media_id != '" + asset3.getId() + "' " + tagName + " = '" + tagValue + "')";
-        System.out.println(ksqlQuery + "1234");
         assetFilter = AssetUtils.getSearchAssetFilter(ksqlQuery, null, null, null, null,
                 null, null);
         listAssetBuilder = AssetService.list(assetFilter, null)
@@ -163,16 +143,27 @@ public class AssetListTests extends BaseTest {
         List<Asset> assets = executor.executeSync(listAssetBuilder).results.getObjects();
         assertThat(assets.size()).isEqualTo(2);
 
-        List<Long> assetIds = assets.stream().map(Asset::getId).collect(Collectors.toList());
-        assertThat(assetIds).contains(asset3.getId(),asset2.getId());
-        assertThat(assetIds).doesNotContain(asset.getId());
+        assertThat(assets).extracting("id").contains(asset3.getId(),asset2.getId()).doesNotContain(asset.getId());
     }
+
 
     // TODO - complete test after ingest util was updated
     @Severity(SeverityLevel.CRITICAL)
     @Description("Asset/action/list - VOD - start with query")
     @Test
     private void listVodAssetsWithStartWithKsqlQuery() {
+
+
+        ksqlQuery = "" + tagName + " ^ '" + tagValue + "'";
+        assetFilter = AssetUtils.getSearchAssetFilter(ksqlQuery, null, null, null, null,
+                null, null);
+        listAssetBuilder = AssetService.list(assetFilter, null)
+                .setKs(BaseTest.SharedHousehold.getSharedMasterUserKs());
+        List<Asset> assets = executor.executeSync(listAssetBuilder).results.getObjects();
+        assertThat(assets.size()).isEqualTo(2);
+
+        assertThat(assets).extracting("id").contains(asset3.getId(),asset2.getId()).doesNotContain(asset.getId());
+
 
     }
 
