c8102e1ca202d97cad184689e3e6a2aa35e2395f
KALTURA\Maksim.Superfin <maksim.superfin@kaltura.com>
2018-06-05 04:19:06 +0300

    Updated util public static void printTestsNameAndDescription()

--- src/test/java/com/kaltura/client/test/utils/InfraUtils.java
+++ src/test/java/com/kaltura/client/test/utils/InfraUtils.java
@@ -3,6 +3,7 @@ package com.kaltura.client.test.utils;
 import com.google.common.reflect.ClassPath;
 import io.qameta.allure.Description;
 import org.apache.commons.io.FileUtils;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -54,7 +55,7 @@ public class InfraUtils {
     }
 
     public static void printTestsNameAndDescription() throws IOException {
-        String packageName = "com.kaltura.client.test.tests.servicesTests";
+        String packageName = "com.kaltura.client.test.tests";
 
         Set<ClassPath.ClassInfo> allClasses = ClassPath.from(ClassLoader.getSystemClassLoader()).getTopLevelClassesRecursive(packageName);
         for (ClassPath.ClassInfo classInfo: allClasses) {
@@ -68,7 +69,20 @@ public class InfraUtils {
                     System.out.println("Method: " + method.getName());
                     System.out.println("Description: " + description.value() + "\n");
                 }
+
+                Test testAnnotation = method.getAnnotation(Test.class);
+                if (testAnnotation != null && !testAnnotation.toString().contains("description=,")) {
+                    System.out.println("Method: " + method.getName());
+                    System.out.println("Description: " + getDescriptionFromTestAnnotation(testAnnotation.toString()) + "\n");
+                }
             }
         }
+    }
+
+    private static String getDescriptionFromTestAnnotation(String testAnnotationContent) {
+        String descriptionEqual = "description=";
+        int descriptionStartsPosition = testAnnotationContent.indexOf(descriptionEqual) + descriptionEqual.length();
+        int descriptionEndsPosition = testAnnotationContent.indexOf(",", descriptionStartsPosition);
+        return testAnnotationContent.substring(descriptionStartsPosition, descriptionEndsPosition);
     }
 }
