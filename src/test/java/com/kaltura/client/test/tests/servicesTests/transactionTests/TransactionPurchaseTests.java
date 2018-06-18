ca1f45d8654214d697d0c91fd9ae14bfe1af5435
KALTURA\michael.Kondratiev <michael.kondratiev@kaltura.com>
2018-06-18 09:38:43 +0300

    transaction fix

--- src/test/java/com/kaltura/client/test/tests/servicesTests/transactionTests/TransactionPurchaseTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/transactionTests/TransactionPurchaseTests.java
@@ -3,7 +3,6 @@ package com.kaltura.client.test.tests.servicesTests.transactionTests;
 import com.kaltura.client.test.tests.BaseTest;
 import com.kaltura.client.test.utils.HouseholdUtils;
 import com.kaltura.client.types.Household;
-import com.kaltura.client.types.HouseholdUser;
 import org.testng.annotations.Test;
 
 public class TransactionPurchaseTests extends BaseTest {
@@ -13,7 +12,7 @@ public class TransactionPurchaseTests extends BaseTest {
     @Test
     private void purchaseDefaultPG() {
         Household household =  HouseholdUtils.createHousehold(1, 1, true);
-        HouseholdUser masterUser =
+//        HouseholdUser masterUser =
 
     }
 
