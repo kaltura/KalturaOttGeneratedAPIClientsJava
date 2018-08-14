88178586d41f8cecca18315f2154aede9b81f04e
alon.basin <alon.basin@kaltura.com>
2018-08-14 17:30:28 +0300

    added tests: purchasePpvTwice(), purchaseCollectionTwice(), purchasePpvWithInvalidPpvModuleId(), purchaseSubscriptionTwice()

--- src/test/java/com/kaltura/client/test/tests/servicesTests/transactionTests/TransactionPurchaseTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/transactionTests/TransactionPurchaseTests.java
@@ -18,6 +18,7 @@ import org.testng.annotations.Test;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import static com.kaltura.client.services.HouseholdService.delete;
 import static com.kaltura.client.services.ProductPriceService.list;
@@ -36,7 +37,7 @@ public class TransactionPurchaseTests extends BaseTest {
     private String userKs;
 
     @BeforeClass()
-    public void transaction_purchas_before_class(){
+    public void transaction_purchase_before_class(){
         // Prepare household with users and devices
         int numOfUsers = 2;
         int numOfDevices = 2;
@@ -196,7 +197,7 @@ public class TransactionPurchaseTests extends BaseTest {
 
         // set product price filter
         ProductPriceFilter productPriceFilter = new ProductPriceFilter();
-        productPriceFilter.setCollectionIdIn(getSharedCommonSubscription().getId());
+        productPriceFilter.setSubscriptionIdIn(getSharedCommonSubscription().getId());
 
         // productPrice list
         Response<ListResponse<ProductPrice>> productPriceResponse = executor.executeSync(list(productPriceFilter)
@@ -322,8 +323,235 @@ public class TransactionPurchaseTests extends BaseTest {
         deleteVod(mediaAsset.getName());
     }
 
+    @Severity(SeverityLevel.NORMAL)
+    @Description("transaction/action/purchase - subscription with invalid price - error 6000")
+    @Test()
+    public void purchaseSubscriptionWithInvalidPrice() {
+        // Prepare household with users and devices
+        Household household = createHousehold(1, 1, true);
+        String masterUserUdid = getDevicesList(household).get(0).getUdid();
+        String masterUserKs = getHouseholdMasterUserKs(household, masterUserUdid);
+
+        // set product price filter
+        ProductPriceFilter productPriceFilter = new ProductPriceFilter();
+        productPriceFilter.setSubscriptionIdIn(getSharedCommonSubscription().getId());
+
+        // productPrice list
+        Response<ListResponse<ProductPrice>> productPriceResponse = executor.executeSync(list(productPriceFilter)
+                .setKs(masterUserKs));
+        assertThat(productPriceResponse.results).isNotNull();
+        assertThat(productPriceResponse.results.getObjects().get(0).getPurchaseStatus()).isEqualTo(PurchaseStatus.FOR_PURCHASE);
+
+        // purchase subscription
+        Purchase purchase = new Purchase();
+        purchase.setProductType(TransactionType.SUBSCRIPTION);
+        purchase.setContentId(0);
+        purchase.setCurrency(productPriceResponse.results.getObjects().get(0).getPrice().getCurrency());
+        purchase.setProductId(Integer.valueOf(getSharedCommonSubscription().getId()));
+        purchase.setPrice(productPriceResponse.results.getObjects().get(0).getPrice().getAmount() + 1);
+
+        Response<Transaction> transactionResponse = executor.executeSync(purchase(purchase)
+                .setKs(masterUserKs));
+
+        // assert transaction
+        assertThat(transactionResponse.results).isNull();
+        assertThat(transactionResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(6000).getCode());
+
+        //cleanup - delete hh
+        executor.executeSync(delete().setKs(masterUserKs));
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("transaction/action/purchase - ppv with invalid ppv moduleId - error 6001")
+    @Test()
+    public void purchasePpvWithInvalidPpvModuleId() {
+        // Prepare household with users and devices
+        Household household = createHousehold(1, 1, true);
+        String masterUserUdid = getDevicesList(household).get(0).getUdid();
+        String masterUserKs = getHouseholdMasterUserKs(household, masterUserUdid);
+
+        // set product price filter
+        ProductPriceFilter productPriceFilter = new ProductPriceFilter();
+        productPriceFilter.setFileIdIn(getSharedWebMediaFile().getId().toString());
+
+        // productPrice list
+        Response<ListResponse<ProductPrice>> productPriceResponse = executor.executeSync(list(productPriceFilter)
+                .setKs(masterUserKs));
+        assertThat(productPriceResponse.results).isNotNull();
+        assertThat(productPriceResponse.results.getObjects().get(0).getPurchaseStatus()).isEqualTo(PurchaseStatus.FOR_PURCHASE);
+
+        // get ppv
+        PpvPrice productPricePpv = (PpvPrice) productPriceResponse.results.getObjects().get(0);
+        assertThat(productPricePpv.getPurchaseStatus()).isEqualTo(PurchaseStatus.FOR_PURCHASE);
+        assertThat(productPricePpv.getFileId()).isEqualTo(getSharedWebMediaFile().getId());
+
+        // purchase ppv
+        Purchase purchase = new Purchase();
+        purchase.setProductType(TransactionType.PPV);
+        purchase.setProductId(1);
+        purchase.setContentId(getSharedWebMediaFile().getId());
+        purchase.setCurrency(productPricePpv.getPrice().getCurrency());
+        purchase.setPrice(productPricePpv.getPrice().getAmount());
+
+        Response<Transaction> transactionResponse = executor.executeSync(purchase(purchase)
+                .setKs(masterUserKs));
+
+        // assert transaction
+        assertThat(transactionResponse.results).isNull();
+        assertThat(transactionResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(6001).getCode());
+
+        //cleanup - delete hh
+        executor.executeSync(delete().setKs(masterUserKs));
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("transaction/action/purchase - same collection twice - error 3021")
+    @Test()
+    public void purchaseCollectionTwice() {
+        // Prepare household with users and devices
+        Household household = createHousehold(2, 2, true);
+        List<HouseholdDevice> devices = getDevicesList(household);
+        String masterUserKs = getHouseholdMasterUserKs(household, devices.get(0).getUdid());
+        String userKs = getHouseholdUserKs(household, devices.get(1).getUdid());
+
+        // set product price filter
+        ProductPriceFilter productPriceFilter = new ProductPriceFilter();
+        productPriceFilter.setCollectionIdIn(getSharedCommonCollection().getId());
+
+        // productPrice list
+        Response<ListResponse<ProductPrice>> productPriceResponse = executor.executeSync(list(productPriceFilter)
+                .setKs(masterUserKs));
+        assertThat(productPriceResponse.results).isNotNull();
+        assertThat(productPriceResponse.results.getObjects().get(0).getPurchaseStatus()).isEqualTo(PurchaseStatus.FOR_PURCHASE);
+
+        // purchase collection
+        Purchase purchase = new Purchase();
+        purchase.setCurrency(productPriceResponse.results.getObjects().get(0).getPrice().getCurrency());
+        purchase.setPrice(productPriceResponse.results.getObjects().get(0).getPrice().getAmount());
+        purchase.setContentId(0);
+        purchase.setProductId(Integer.valueOf(getSharedCommonCollection().getId()));
+        purchase.setProductType(TransactionType.COLLECTION);
+
+        // purchase collection - first time
+        executor.executeSync(purchase(purchase)
+                .setKs(masterUserKs));
+
+        // purchase collection - second time
+        Response<Transaction> transactionResponse = executor.executeSync(purchase(purchase)
+                .setKs(userKs));
+
+        // assert transaction
+        assertThat(transactionResponse.results).isNull();
+        assertThat(transactionResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(3027).getCode());
+
+        //cleanup - delete hh
+        executor.executeSync(delete().setKs(masterUserKs));
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("transaction/action/purchase - same ppv twice - error 3021")
+    @Test()
+    public void purchasePpvTwice() {
+        // Prepare household with users and devices
+        Household household = createHousehold(2, 2, true);
+        List<HouseholdDevice> devices = getDevicesList(household);
+        String masterUserKs = getHouseholdMasterUserKs(household, devices.get(0).getUdid());
+        String userKs = getHouseholdUserKs(household, devices.get(1).getUdid());
+
+        // set product price filter
+        ProductPriceFilter productPriceFilter = new ProductPriceFilter();
+        productPriceFilter.setFileIdIn(getSharedWebMediaFile().getId().toString());
+
+        // productPrice list
+        Response<ListResponse<ProductPrice>> productPriceResponse = executor.executeSync(list(productPriceFilter)
+                .setKs(masterUserKs));
+        assertThat(productPriceResponse.results).isNotNull();
+        assertThat(productPriceResponse.results.getObjects().get(0).getPurchaseStatus()).isEqualTo(PurchaseStatus.FOR_PURCHASE);
+
+        // get ppv
+        PpvPrice productPricePpv = (PpvPrice) productPriceResponse.results.getObjects().get(0);
+        assertThat(productPricePpv.getPurchaseStatus()).isEqualTo(PurchaseStatus.FOR_PURCHASE);
+        assertThat(productPricePpv.getFileId()).isEqualTo(getSharedWebMediaFile().getId());
+
+        // purchase ppv
+        Purchase purchase = new Purchase();
+        purchase.setProductType(TransactionType.PPV);
+        purchase.setProductId(Integer.valueOf(productPricePpv.getPpvModuleId()));
+        purchase.setContentId(getSharedWebMediaFile().getId());
+        purchase.setCurrency(productPricePpv.getPrice().getCurrency());
+        purchase.setPrice(productPricePpv.getPrice().getAmount());
+
+        // purchase ppv - first time
+        executor.executeSync(purchase(purchase)
+                .setKs(masterUserKs));
+
+        // purchase ppv - second time
+        Response<Transaction> transactionResponse = executor.executeSync(purchase(purchase)
+                .setKs(userKs));
+
+        // assert transaction
+        assertThat(transactionResponse.results).isNull();
+        assertThat(transactionResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(3021).getCode());
+
+        //cleanup - delete hh
+        executor.executeSync(delete().setKs(masterUserKs));
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("transaction/action/purchase - same subscription twice - error 3024")
+    @Test()
+    public void purchaseSubscriptionTwice() {
+        // Prepare household with users and devices
+        Household household = createHousehold(2, 2, true);
+        List<HouseholdDevice> devices = getDevicesList(household);
+        String masterUserKs = getHouseholdMasterUserKs(household, devices.get(0).getUdid());
+        String userKs = getHouseholdUserKs(household, devices.get(1).getUdid());
+
+        // set product price filter
+        ProductPriceFilter productPriceFilter = new ProductPriceFilter();
+        productPriceFilter.setSubscriptionIdIn(getSharedCommonSubscription().getId());
+
+        // productPrice list
+        Response<ListResponse<ProductPrice>> productPriceResponse = executor.executeSync(list(productPriceFilter)
+                .setKs(masterUserKs));
+        assertThat(productPriceResponse.results).isNotNull();
+        assertThat(productPriceResponse.results.getObjects().get(0).getPurchaseStatus()).isEqualTo(PurchaseStatus.FOR_PURCHASE);
+
+        // purchase subscription
+        Purchase purchase = new Purchase();
+        purchase.setProductType(TransactionType.SUBSCRIPTION);
+        purchase.setContentId(0);
+        purchase.setCurrency(productPriceResponse.results.getObjects().get(0).getPrice().getCurrency());
+        purchase.setProductId(Integer.valueOf(getSharedCommonSubscription().getId()));
+        purchase.setPrice(productPriceResponse.results.getObjects().get(0).getPrice().getAmount());
+
+        // purchase subscription - first time
+        executor.executeSync(purchase(purchase)
+                .setKs(masterUserKs));
+
+        // purchase subscription - second time - because of beo-5249
+        executor.executeSync(purchase(purchase)
+                .setKs(masterUserKs));
+
+        // purchase subscription - third time
+        Response<Transaction> transactionResponse = executor.executeSync(purchase(purchase)
+                .setKs(userKs));
+
+        // assert transaction
+        assertThat(transactionResponse.results).isNull();
+        assertThat(transactionResponse.error.getCode()).isEqualTo(getAPIExceptionFromList(3024).getCode());
+
+        //cleanup - delete hh
+        executor.executeSync(delete().setKs(masterUserKs));
+    }
+
+    // TODO: 8/14/2018 purchase free product
+    // TODO: 8/14/2018 purchase with coupon (also not valid coupon)
+    //  <error name="NotForPurchase" code="3025" description="The Content ID entered is not available for purchase."/>
+    //  <error name="FileToMediaMismatch" code="3028" description="The file and media don't match"/>
+
     @AfterClass
-    public void transaction_purchas_after_class() {
+    public void transaction_purchase_after_class() {
         // cleanup
         executor.executeSync(delete().setKs(masterUserKs));
     }
