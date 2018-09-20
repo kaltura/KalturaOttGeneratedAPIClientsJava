f18bbe51bb643071e344294a1baa132591c33fe4
KALTURA\shmulik.kummer <shmulik.kummer@kaltura.com>
2018-09-20 15:09:18 +0300

    Updated class - "CountryListTests"

--- src/test/java/com/kaltura/client/test/tests/servicesTests/countryList/CountryListTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/countryList/CountryListTests.java
@@ -1,6 +1,8 @@
 package com.kaltura.client.test.tests.servicesTests.countryList;
 
+import com.kaltura.client.enums.CountryOrderBy;
 import com.kaltura.client.test.tests.BaseTest;
+import com.kaltura.client.types.Collection;
 import com.kaltura.client.types.Country;
 import com.kaltura.client.types.CountryFilter;
 import com.kaltura.client.types.ListResponse;
@@ -10,7 +12,11 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.Test;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 import static com.kaltura.client.services.CountryService.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CountryListTests extends BaseTest {
 
@@ -25,9 +31,31 @@ public class CountryListTests extends BaseTest {
         CountryFilter countryFilter = new CountryFilter();
         ListCountryBuilder listCountryBuilder = new ListCountryBuilder(countryFilter)
                 .setKs(getOperatorKs());
-        Response<ListResponse<Country>> countryResponse = executor.executeSync(listCountryBuilder);
 
+        // country/action/list
+        Response<ListResponse<Country>> countryResponse = executor.executeSync(listCountryBuilder);
+        assertThat(countryResponse.error).isNull();
+        assertThat(countryResponse.results.getTotalCount()).isGreaterThan(200);
     }
 
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("country/action/list - verify results order (by name)")
+    @Test
+    private void listCountryOrder() {
 
+        CountryFilter countryFilter = new CountryFilter();
+        countryFilter.setOrderBy(CountryOrderBy.NAME_ASC.toString());
+        ListCountryBuilder listCountryBuilder = new ListCountryBuilder(countryFilter)
+                .setKs(getOperatorKs());
+
+        // country/action/list
+        Response<ListResponse<Country>> countryResponse = executor.executeSync(listCountryBuilder);
+        List<String> newList = new ArrayList<>();
+        for (Country country: countryResponse.results.getObjects()) {
+            newList.add(country.getName());
+        }
+        ArrayList<String> temp = new ArrayList<>(newList);
+        Collections.sort(temp);
+        assertThat(temp).isEqualTo(newList);
+    }
 }
