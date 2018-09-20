3abcb40b911c8a14b3fb686007f1b8ccfd47047c
KALTURA\shmulik.kummer <shmulik.kummer@kaltura.com>
2018-09-20 10:51:52 +0300

    Updated tc - "CountryListTests"

--- src/test/java/com/kaltura/client/test/tests/servicesTests/countryList/CountryListTests.java
+++ src/test/java/com/kaltura/client/test/tests/servicesTests/countryList/CountryListTests.java
@@ -1,4 +1,33 @@
 package com.kaltura.client.test.tests.servicesTests.countryList;
 
-public class CountryListTests {
+import com.kaltura.client.test.tests.BaseTest;
+import com.kaltura.client.types.Country;
+import com.kaltura.client.types.CountryFilter;
+import com.kaltura.client.types.ListResponse;
+import com.kaltura.client.utils.response.base.Response;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.Test;
+
+import static com.kaltura.client.services.CountryService.*;
+
+public class CountryListTests extends BaseTest {
+
+    private void list_tests_before_class() {
+
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("country/action/list - empty filter - get all countries")
+    @Test
+    private void listCountry() {
+        CountryFilter countryFilter = new CountryFilter();
+        ListCountryBuilder listCountryBuilder = new ListCountryBuilder(countryFilter)
+                .setKs(getOperatorKs());
+        Response<ListResponse<Country>> countryResponse = executor.executeSync(listCountryBuilder);
+
+    }
+
+
 }
