340ec68eee32a719e952f808ba600a237f6f4aca
KALTURA\Maksim.Superfin <maksim.superfin@kaltura.com>
2018-09-25 09:46:49 +0300

    commented useless tests. Added shared data creation.

--- src/test/java/com/kaltura/client/test/tests/featuresTests/versions/five_zero_three/ParentChildMetadataInheritanceTests.java
+++ src/test/java/com/kaltura/client/test/tests/featuresTests/versions/five_zero_three/ParentChildMetadataInheritanceTests.java
@@ -25,6 +25,7 @@ import static com.kaltura.client.test.tests.enums.IngestAction.INSERT;
 import static com.kaltura.client.test.tests.enums.MediaType.MOVIE;
 import static com.kaltura.client.test.utils.AssetStructMetaUtils.getAssetStructMeta;
 import static com.kaltura.client.test.utils.AssetStructUtils.getAssetStruct;
+import static com.kaltura.client.test.utils.BaseUtils.getCurrentDateInFormat;
 import static com.kaltura.client.test.utils.dbUtils.DBUtils.getMetaIdByName;
 import static com.kaltura.client.test.utils.ingestUtils.IngestVodOpcUtils.getVodData;
 import static com.kaltura.client.test.utils.ingestUtils.IngestVodUtils.deleteVod;
@@ -41,9 +42,11 @@ public class ParentChildMetadataInheritanceTests extends BaseTest {
 
     private String metaIds;
     private Response<ListResponse<AssetStruct>> sharedAssetStructListResponse;
+    private AssetStruct sharedAssetStruct1, sharedAssetStruct2;
 
     @BeforeClass()
     public void setUp() {
+        // TODO: check if assetStructList be useful at the end
         // assetStructList to fill default metaIds
         AssetStructFilter filter = new AssetStructFilter();
         ListAssetStructBuilder listAssetStructBuilder = AssetStructService.list(filter);
@@ -51,6 +54,23 @@ public class ParentChildMetadataInheritanceTests extends BaseTest {
                 .setKs(getOperatorKs()));
         assertThat(sharedAssetStructListResponse.results.getTotalCount()).isGreaterThan(0);
         metaIds = sharedAssetStructListResponse.results.getObjects().get(0).getMetaIds();
+
+        // create shared assetStruct1
+        String prefix = "AssetStruct_" + getCurrentDateInFormat("yyMMddHHmmss");
+        sharedAssetStruct1 = getAssetStruct(prefix, "eng", false, metaIds, null, null, null);
+        AddAssetStructBuilder addAssetStructBuilder = AssetStructService.add(sharedAssetStruct1);
+        Response<AssetStruct> assetStructResponse = executor.executeSync(addAssetStructBuilder
+                .setKs(getOperatorKs()).setLanguage("*"));
+        sharedAssetStruct1 = assetStructResponse.results;
+        assertThat(sharedAssetStruct1.getSystemName()).isEqualToIgnoringCase(prefix + "_System_name");
+        // create shared assetStruct2
+        prefix = "AssetStruct_" + getCurrentDateInFormat("yyMMddHHmmss");
+        sharedAssetStruct2 = getAssetStruct(prefix, "eng", false, metaIds, null, null, null);
+        addAssetStructBuilder = AssetStructService.add(sharedAssetStruct2);
+        assetStructResponse = executor.executeSync(addAssetStructBuilder
+                .setKs(getOperatorKs()).setLanguage("*"));
+        sharedAssetStruct2 = assetStructResponse.results;
+        assertThat(sharedAssetStruct2.getSystemName()).isEqualToIgnoringCase(prefix + "_System_name");
     }
 
     @Test
@@ -73,6 +93,11 @@ public class ParentChildMetadataInheritanceTests extends BaseTest {
 
     @Test
     public void testCreateInheritance() {
+        // add assetStruct1 with fields to connect
+        // add assetStruct2 with fields to connect
+        // check fields have link as it can be done in API and additionally on Tvm
+
+
         List<AssetStruct> assetStructs = loadAssetStructsWithoutHierarchy(3, sharedAssetStructListResponse.results.getObjects());
         AssetStruct assetStructGrandParent = assetStructs.get(0);
         AssetStruct assetStructParent = assetStructs.get(1);
@@ -102,129 +127,139 @@ public class ParentChildMetadataInheritanceTests extends BaseTest {
     }
 
     // added to play with methods that are going to be checked
-    @Test
-    public void sandbox() {
-        String prefix = "MaxTest_assetStruct_";
+//    @Test
+//    public void sandbox() {
+//        String prefix = "MaxTest_assetStruct_";
+//
+//        Long assetStructId = sharedAssetStructListResponse.results.getObjects().get(0).getId();
+//
+//        AssetStructMetaFilter assetStructMetaFilter = new AssetStructMetaFilter();
+//        assetStructMetaFilter.setAssetStructIdEqual(assetStructId);
+//        ListAssetStructMetaBuilder listAssetStructMetaBuilder = AssetStructMetaService.list(assetStructMetaFilter);
+//        Response<ListResponse<AssetStructMeta>> listAssetMetaStructResponse = executor.executeSync(listAssetStructMetaBuilder
+//                .setKs(getOperatorKs()));
+//        assertThat(listAssetMetaStructResponse.results.getTotalCount()).isGreaterThan(0);
+//        Long metaId = listAssetMetaStructResponse.results.getObjects().get(0).getMetaId();
+//
+//        String defaultIngestValue = prefix + "_defaultIngestValue";
+//        String ingestReferencePath = prefix + "ingestReferencePath";
+//        boolean isProtectFromIngest = false;
+//        boolean isInherited = true;
+//        AssetStructMeta assetStructMeta = getAssetStructMeta(defaultIngestValue,
+//                ingestReferencePath, isProtectFromIngest, isInherited);
+//        UpdateAssetStructMetaBuilder updateAssetStructMetaBuilder =
+//                new UpdateAssetStructMetaBuilder(assetStructId, metaId, assetStructMeta);
+//        Response<AssetStructMeta> assetStructMetaResponse = executor.executeSync(updateAssetStructMetaBuilder
+//                .setKs(getOperatorKs()));
+//        assertThat(assetStructMetaResponse.results.getAssetStructId()).isEqualTo(assetStructId);
+//        assertThat(assetStructMetaResponse.results.getMetaId()).isEqualTo(metaId);
+//        assertThat(assetStructMetaResponse.results.getDefaultIngestValue()).isEqualToIgnoringCase(defaultIngestValue);
+//        assertThat(assetStructMetaResponse.results.getIngestReferencePath()).isEqualToIgnoringCase(ingestReferencePath);
+//        assertThat(assetStructMetaResponse.results.getProtectFromIngest()).isEqualTo(isProtectFromIngest);
+//        //TODO: update library to have options compare it
+//        //assertThat(assetStructMetaResponse.results.getInherited()).isEqualTo(isInherited);
+//
+//        // assetStructAdd
+//        AssetStruct assetStruct = getAssetStruct(prefix, "eng", false, metaIds, null, null, null);
+//        AddAssetStructBuilder addAssetStructBuilder = AssetStructService.add(assetStruct);
+//        Response<AssetStruct> assetStructResponse = executor.executeSync(addAssetStructBuilder
+//                .setKs(getOperatorKs()).setLanguage("*"));
+//        AssetStruct assetStructFromResponse = assetStructResponse.results;
+//        assertThat(assetStructResponse.results.getSystemName()).isEqualToIgnoringCase(prefix + "_System_name");
+//
+//        assetStruct.setSystemName(prefix + "_System_name_upd");
+//
+//        // assetStructUpdate
+//        UpdateAssetStructBuilder updateAssetStructBuilder = AssetStructService.update(assetStructFromResponse.getId(), assetStruct);
+//        assetStructResponse = executor.executeSync(updateAssetStructBuilder
+//                .setKs(getOperatorKs())
+//                .setLanguage("*"));
+//        assertThat(assetStructResponse.results.getSystemName()).isEqualToIgnoringCase(prefix + "_System_name_upd");
+//
+//        // assetStructDelete
+//        DeleteAssetStructBuilder deleteAssetStructBuilder = AssetStructService.delete(assetStructFromResponse.getId());
+//        Response<Boolean> deleteAssetStructResponse = executor.executeSync(deleteAssetStructBuilder
+//                .setKs(getOperatorKs()));
+//        assertThat(deleteAssetStructResponse.results.booleanValue()).isEqualTo(true);
+//    }
 
-        Long assetStructId = sharedAssetStructListResponse.results.getObjects().get(0).getId();
+//    @Test
+//    public void sandboxRemoveMetasAndTags() {
+//        IngestVodUtils.VodData vodData = getVodData(MOVIE, INSERT);
+//        MediaAsset movie = insertVod(vodData, true);
+//
+//        assertThat(movie.getName()).isEqualTo(movie.getExternalId());
+//        assertThat(movie.getDescription()).isEqualTo("description of " + movie.getExternalId());
+//        // get all metas
+//        Set<String> movieMetasNames = movie.getMetas().keySet();
+//        int metasSizeAfterCreation = movieMetasNames.size();
+//        int nonBasicMediaMetasCount = 0;
+//        assertThat(metasSizeAfterCreation).isGreaterThan(0);
+//
+//        // get all tags
+//        Set<String> movieTagsNames = movie.getTags().keySet();
+//        int tagsSizeAfterCreation = movieTagsNames.size();
+//        assertThat(tagsSizeAfterCreation).isGreaterThan(0);
+//
+//        // get all metas and tags info to remove them from media
+//        List<String> metaIds = new ArrayList<>();
+//        int metaId;
+//        for (String metaName: movieMetasNames) {
+//            metaId = getMetaIdByName(metaName, false);
+//            if (metaId !=-1) {
+//                metaIds.add(String.valueOf(metaId));
+//                nonBasicMediaMetasCount++;
+//            }
+//        }
+//        for (String metaName: movieTagsNames) {
+//            metaId = getMetaIdByName(metaName, true);
+//            if (metaId !=-1) {
+//                metaIds.add(String.valueOf(metaId));
+//            }
+//        }
+//        String metaIdsIn = String.join(",", metaIds);
+//
+//        // removeMetasAndTags
+//        RemoveMetasAndTagsAssetBuilder removeMetasAndTagsAssetBuilder =
+//                AssetService.removeMetasAndTags(movie.getId(), AssetReferenceType.MEDIA, metaIdsIn);
+//        Response<Boolean> booleanResponse = executor.executeSync(removeMetasAndTagsAssetBuilder
+//                .setKs(getOperatorKs()));
+//        assertThat(booleanResponse.results).isEqualTo(true);
+//
+//        // check results
+//        GetAssetBuilder getAssetBuilder = AssetService.get(movie.getId().toString(), AssetReferenceType.MEDIA);
+//        Response<Asset> assetResponse = executor.executeSync(getAssetBuilder
+//                .setKs(getOperatorKs()));
+//        movie = (MediaAsset) assetResponse.results;
+//
+//        assertThat(movie.getName()).isEqualTo(movie.getExternalId());
+//        assertThat(movie.getDescription()).isEqualTo("description of " + movie.getExternalId());
+//        assertThat(movie.getMetas().keySet().size()).isEqualTo(metasSizeAfterCreation - nonBasicMediaMetasCount);
+//        assertThat(movie.getTags().keySet().size()).isEqualTo(0);
+//
+//        // cleanup
+//        deleteVod(movie.getExternalId());
+//    }
 
-        AssetStructMetaFilter assetStructMetaFilter = new AssetStructMetaFilter();
-        assetStructMetaFilter.setAssetStructIdEqual(assetStructId);
-        ListAssetStructMetaBuilder listAssetStructMetaBuilder = AssetStructMetaService.list(assetStructMetaFilter);
-        Response<ListResponse<AssetStructMeta>> listAssetMetaStructResponse = executor.executeSync(listAssetStructMetaBuilder
-                .setKs(getOperatorKs()));
-        assertThat(listAssetMetaStructResponse.results.getTotalCount()).isGreaterThan(0);
-        Long metaId = listAssetMetaStructResponse.results.getObjects().get(0).getMetaId();
-
-        String defaultIngestValue = prefix + "_defaultIngestValue";
-        String ingestReferencePath = prefix + "ingestReferencePath";
-        boolean isProtectFromIngest = false;
-        boolean isInherited = true;
-        AssetStructMeta assetStructMeta = getAssetStructMeta(defaultIngestValue,
-                ingestReferencePath, isProtectFromIngest, isInherited);
-        UpdateAssetStructMetaBuilder updateAssetStructMetaBuilder =
-                new UpdateAssetStructMetaBuilder(assetStructId, metaId, assetStructMeta);
-        Response<AssetStructMeta> assetStructMetaResponse = executor.executeSync(updateAssetStructMetaBuilder
-                .setKs(getOperatorKs()));
-        assertThat(assetStructMetaResponse.results.getAssetStructId()).isEqualTo(assetStructId);
-        assertThat(assetStructMetaResponse.results.getMetaId()).isEqualTo(metaId);
-        assertThat(assetStructMetaResponse.results.getDefaultIngestValue()).isEqualToIgnoringCase(defaultIngestValue);
-        assertThat(assetStructMetaResponse.results.getIngestReferencePath()).isEqualToIgnoringCase(ingestReferencePath);
-        assertThat(assetStructMetaResponse.results.getProtectFromIngest()).isEqualTo(isProtectFromIngest);
-        //TODO: update library to have options compare it
-        //assertThat(assetStructMetaResponse.results.getInherited()).isEqualTo(isInherited);
-
-        // assetStructAdd
-        AssetStruct assetStruct = getAssetStruct(prefix, "eng", false, metaIds, null, null, null);
-        AddAssetStructBuilder addAssetStructBuilder = AssetStructService.add(assetStruct);
-        Response<AssetStruct> assetStructResponse = executor.executeSync(addAssetStructBuilder
-                .setKs(getOperatorKs()).setLanguage("*"));
-        AssetStruct assetStructFromResponse = assetStructResponse.results;
-        assertThat(assetStructResponse.results.getSystemName()).isEqualToIgnoringCase(prefix + "_System_name");
-
-        assetStruct.setSystemName(prefix + "_System_name_upd");
-
-        // assetStructUpdate
-        UpdateAssetStructBuilder updateAssetStructBuilder = AssetStructService.update(assetStructFromResponse.getId(), assetStruct);
-        assetStructResponse = executor.executeSync(updateAssetStructBuilder
-                .setKs(getOperatorKs())
-                .setLanguage("*"));
-        assertThat(assetStructResponse.results.getSystemName()).isEqualToIgnoringCase(prefix + "_System_name_upd");
-
-        // assetStructDelete
-        DeleteAssetStructBuilder deleteAssetStructBuilder = AssetStructService.delete(assetStructFromResponse.getId());
-        Response<Boolean> deleteAssetStructResponse = executor.executeSync(deleteAssetStructBuilder
-                .setKs(getOperatorKs()));
-        assertThat(deleteAssetStructResponse.results.booleanValue()).isEqualTo(true);
-    }
-
-    @Test
-    public void sandboxRemoveMetasAndTags() {
-        IngestVodUtils.VodData vodData = getVodData(MOVIE, INSERT);
-        MediaAsset movie = insertVod(vodData, true);
-
-        assertThat(movie.getName()).isEqualTo(movie.getExternalId());
-        assertThat(movie.getDescription()).isEqualTo("description of " + movie.getExternalId());
-        // get all metas
-        Set<String> movieMetasNames = movie.getMetas().keySet();
-        int metasSizeAfterCreation = movieMetasNames.size();
-        int nonBasicMediaMetasCount = 0;
-        assertThat(metasSizeAfterCreation).isGreaterThan(0);
-
-        // get all tags
-        Set<String> movieTagsNames = movie.getTags().keySet();
-        int tagsSizeAfterCreation = movieTagsNames.size();
-        assertThat(tagsSizeAfterCreation).isGreaterThan(0);
-
-        // get all metas and tags info to remove them from media
-        List<String> metaIds = new ArrayList<>();
-        int metaId;
-        for (String metaName: movieMetasNames) {
-            metaId = getMetaIdByName(metaName, false);
-            if (metaId !=-1) {
-                metaIds.add(String.valueOf(metaId));
-                nonBasicMediaMetasCount++;
-            }
-        }
-        for (String metaName: movieTagsNames) {
-            metaId = getMetaIdByName(metaName, true);
-            if (metaId !=-1) {
-                metaIds.add(String.valueOf(metaId));
-            }
-        }
-        String metaIdsIn = String.join(",", metaIds);
-
-        // removeMetasAndTags
-        RemoveMetasAndTagsAssetBuilder removeMetasAndTagsAssetBuilder =
-                AssetService.removeMetasAndTags(movie.getId(), AssetReferenceType.MEDIA, metaIdsIn);
-        Response<Boolean> booleanResponse = executor.executeSync(removeMetasAndTagsAssetBuilder
-                .setKs(getOperatorKs()));
-        assertThat(booleanResponse.results).isEqualTo(true);
-
-        // check results
-        GetAssetBuilder getAssetBuilder = AssetService.get(movie.getId().toString(), AssetReferenceType.MEDIA);
-        Response<Asset> assetResponse = executor.executeSync(getAssetBuilder
-                .setKs(getOperatorKs()));
-        movie = (MediaAsset) assetResponse.results;
-
-        assertThat(movie.getName()).isEqualTo(movie.getExternalId());
-        assertThat(movie.getDescription()).isEqualTo("description of " + movie.getExternalId());
-        assertThat(movie.getMetas().keySet().size()).isEqualTo(metasSizeAfterCreation - nonBasicMediaMetasCount);
-        assertThat(movie.getTags().keySet().size()).isEqualTo(0);
-
-        // cleanup
-        deleteVod(movie.getExternalId());
-    }
-
-    @Test
-    public void sandboxDelete() {
-        DeleteAssetStructBuilder deleteAssetStructBuilder = AssetStructService.delete(2381);
-        Response<Boolean> deleteAssetStructResponse = executor.executeSync(deleteAssetStructBuilder
-                .setKs(getOperatorKs()));
-        assertThat(deleteAssetStructResponse.results.booleanValue()).isEqualTo(true);
-    }
+//    @Test
+//    public void sandboxDelete() {
+//        DeleteAssetStructBuilder deleteAssetStructBuilder = AssetStructService.delete(2381);
+//        Response<Boolean> deleteAssetStructResponse = executor.executeSync(deleteAssetStructBuilder
+//                .setKs(getOperatorKs()));
+//        assertThat(deleteAssetStructResponse.results.booleanValue()).isEqualTo(true);
+//    }
 
     @AfterClass()
     public void tearDown() {
+        // cleaning created shared assetStructs
+        DeleteAssetStructBuilder deleteAssetStructBuilder = AssetStructService.delete(sharedAssetStruct1.getId());
+        Response<Boolean> deleteAssetStructResponse = executor.executeSync(deleteAssetStructBuilder
+                .setKs(getOperatorKs()));
+        assertThat(deleteAssetStructResponse.results.booleanValue()).isEqualTo(true);
+
+        deleteAssetStructBuilder = AssetStructService.delete(sharedAssetStruct2.getId());
+        deleteAssetStructResponse = executor.executeSync(deleteAssetStructBuilder
+                .setKs(getOperatorKs()));
+        assertThat(deleteAssetStructResponse.results.booleanValue()).isEqualTo(true);
     }
 }
