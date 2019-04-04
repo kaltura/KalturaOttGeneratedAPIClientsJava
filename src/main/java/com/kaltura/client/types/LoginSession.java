22ffea0e2de431a1817fed0054f7156145d80096
Backend CI <ott.rnd.core@kaltura.com>
2019-04-04 00:07:29 +0000

    Auto-generated by Jenkins

--- src/main/java/com/kaltura/client/types/LoginSession.java
+++ src/main/java/com/kaltura/client/types/LoginSession.java
@@ -49,12 +49,17 @@ public class LoginSession extends ObjectBase {
 	
 	public interface Tokenizer extends ObjectBase.Tokenizer {
 		String ks();
+		String expiration();
 	}
 
 	/**
 	 * Access token in a KS format
 	 */
 	private String ks;
+	/**
+	 * Expiration
+	 */
+	private Long expiration;
 
 	// ks:
 	public String getKs(){
@@ -66,6 +71,18 @@ public class LoginSession extends ObjectBase {
 
 	public void ks(String multirequestToken){
 		setToken("ks", multirequestToken);
+	}
+
+	// expiration:
+	public Long getExpiration(){
+		return this.expiration;
+	}
+	public void setExpiration(Long expiration){
+		this.expiration = expiration;
+	}
+
+	public void expiration(String multirequestToken){
+		setToken("expiration", multirequestToken);
 	}
 
 
@@ -80,6 +97,7 @@ public class LoginSession extends ObjectBase {
 
 		// set members values:
 		ks = GsonParser.parseString(jsonObject.get("ks"));
+		expiration = GsonParser.parseLong(jsonObject.get("expiration"));
 
 	}
 
@@ -87,6 +105,7 @@ public class LoginSession extends ObjectBase {
 		Params kparams = super.toParams();
 		kparams.add("objectType", "KalturaLoginSession");
 		kparams.add("ks", this.ks);
+		kparams.add("expiration", this.expiration);
 		return kparams;
 	}
 
