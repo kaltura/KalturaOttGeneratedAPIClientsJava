f6f9ff3b69114ff82e6242cb408fc9e906eca977
Backend CI <ott.rnd.core@kaltura.com>
2021-02-28 06:36:45 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/393, branch BEO-9453-epg-invalidation

--- src/main/java/com/kaltura/client/Client.java
+++ src/main/java/com/kaltura/client/Client.java
@@ -44,8 +44,8 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:21-02-24");
-		this.setApiVersion("6.1.0.28923");
+		this.setClientTag("java:21-02-28");
+		this.setApiVersion("6.1.0.28931");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
