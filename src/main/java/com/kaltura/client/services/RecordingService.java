abcd75991b95e81993c22f87b7e893f37d58779c
Backend CI <ott.rnd.core@kaltura.com>
2023-01-16 16:12:49 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1031, branch 8_4_1

--- src/main/java/com/kaltura/client/services/RecordingService.java
+++ src/main/java/com/kaltura/client/services/RecordingService.java
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2022  Kaltura Inc.
+// Copyright (C) 2006-2023  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -29,6 +29,7 @@ package com.kaltura.client.services;
 
 import com.kaltura.client.types.ActionResult;
 import com.kaltura.client.types.FilterPager;
+import com.kaltura.client.types.ImmediateRecording;
 import com.kaltura.client.types.Recording;
 import com.kaltura.client.types.RecordingFilter;
 import com.kaltura.client.utils.request.ArrayRequestBuilder;
@@ -150,6 +151,39 @@ public class RecordingService {
 		return new GetRecordingBuilder(id);
 	}
 	
+	public static class ImmediateRecordRecordingBuilder extends RequestBuilder<ImmediateRecording, ImmediateRecording.Tokenizer, ImmediateRecordRecordingBuilder> {
+		
+		public ImmediateRecordRecordingBuilder(long assetId, long epgChannelId, int endPadding) {
+			super(ImmediateRecording.class, "recording", "immediateRecord");
+			params.add("assetId", assetId);
+			params.add("epgChannelId", epgChannelId);
+			params.add("endPadding", endPadding);
+		}
+		
+		public void assetId(String multirequestToken) {
+			params.add("assetId", multirequestToken);
+		}
+		
+		public void epgChannelId(String multirequestToken) {
+			params.add("epgChannelId", multirequestToken);
+		}
+		
+		public void endPadding(String multirequestToken) {
+			params.add("endPadding", multirequestToken);
+		}
+	}
+
+	/**
+	 * Immediate Record
+	 * 
+	 * @param assetId asset identifier
+	 * @param epgChannelId epg channel identifier
+	 * @param endPadding end padding offset
+	 */
+    public static ImmediateRecordRecordingBuilder immediateRecord(long assetId, long epgChannelId, int endPadding)  {
+		return new ImmediateRecordRecordingBuilder(assetId, epgChannelId, endPadding);
+	}
+	
 	public static class ListRecordingBuilder extends ListResponseRequestBuilder<Recording, Recording.Tokenizer, ListRecordingBuilder> {
 		
 		public ListRecordingBuilder(RecordingFilter filter, FilterPager pager) {
@@ -198,6 +232,39 @@ public class RecordingService {
 	 */
     public static ProtectRecordingBuilder protect(long id)  {
 		return new ProtectRecordingBuilder(id);
+	}
+	
+	public static class StopRecordingBuilder extends RequestBuilder<Recording, Recording.Tokenizer, StopRecordingBuilder> {
+		
+		public StopRecordingBuilder(long assetId, long epgChannelId, long householdRecordingId) {
+			super(Recording.class, "recording", "stop");
+			params.add("assetId", assetId);
+			params.add("epgChannelId", epgChannelId);
+			params.add("householdRecordingId", householdRecordingId);
+		}
+		
+		public void assetId(String multirequestToken) {
+			params.add("assetId", multirequestToken);
+		}
+		
+		public void epgChannelId(String multirequestToken) {
+			params.add("epgChannelId", multirequestToken);
+		}
+		
+		public void householdRecordingId(String multirequestToken) {
+			params.add("householdRecordingId", multirequestToken);
+		}
+	}
+
+	/**
+	 * Stop ongoing household recording
+	 * 
+	 * @param assetId asset identifier
+	 * @param epgChannelId epg channel identifier
+	 * @param householdRecordingId household recording identifier
+	 */
+    public static StopRecordingBuilder stop(long assetId, long epgChannelId, long householdRecordingId)  {
+		return new StopRecordingBuilder(assetId, epgChannelId, householdRecordingId);
 	}
 	
 	public static class UpdateRecordingBuilder extends RequestBuilder<Recording, Recording.Tokenizer, UpdateRecordingBuilder> {
