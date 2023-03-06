8a7a7ac6125bc780a2059a9533d40bbb931319d9
Backend CI <ott.rnd.core@kaltura.com>
2023-03-06 09:16:11 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1071, branch BEO-13538_Promotions_Shop_Managers_Campaign

--- src/main/java/com/kaltura/client/Client.java
+++ src/main/java/com/kaltura/client/Client.java
@@ -44,8 +44,8 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:23-02-28");
-		this.setApiVersion("8.5.1.30300");
+		this.setClientTag("java:23-03-06");
+		this.setApiVersion("8.5.1.30301");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
