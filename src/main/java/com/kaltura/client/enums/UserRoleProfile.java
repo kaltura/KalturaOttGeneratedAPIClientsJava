bdf2c522d8be2937f46b6208d6f6a4a0085efe59
Backend CI <ott.rnd.core@kaltura.com>
2023-04-13 08:29:41 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1086, branch BEO-13830_bypass_cache_part_2

--- src/main/java/com/kaltura/client/enums/UserRoleProfile.java
+++ src/main/java/com/kaltura/client/enums/UserRoleProfile.java
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2022  Kaltura Inc.
+// Copyright (C) 2006-2023  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -37,7 +37,8 @@ public enum UserRoleProfile implements EnumAsString {
 	USER("USER"),
 	PARTNER("PARTNER"),
 	PROFILE("PROFILE"),
-	SYSTEM("SYSTEM");
+	SYSTEM("SYSTEM"),
+	PERMISSION_EMBEDDED("PERMISSION_EMBEDDED");
 
 	private String value;
 
