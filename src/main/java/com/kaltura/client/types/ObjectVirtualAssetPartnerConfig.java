858f91384078b0910f0e0ba0e3db989ea30a21c0
Backend CI <ott.rnd.core@kaltura.com>
2024-12-30 19:20:51 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1386, branch 10.7.0.5

--- src/main/java/com/kaltura/client/types/ObjectVirtualAssetPartnerConfig.java
+++ src/main/java/com/kaltura/client/types/ObjectVirtualAssetPartnerConfig.java
@@ -32,6 +32,7 @@ import com.kaltura.client.Params;
 import com.kaltura.client.utils.GsonParser;
 import com.kaltura.client.utils.request.MultiRequestBuilder;
 import com.kaltura.client.utils.request.RequestBuilder;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
