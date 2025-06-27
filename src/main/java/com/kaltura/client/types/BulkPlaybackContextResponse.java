758726c88f18bb5afb5891a50145bbeb39357bde
Backend CI <ott.rnd.core@kaltura.com>
2025-06-27 15:34:41 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1508, branch BEO-17389

---
+++ src/main/java/com/kaltura/client/types/BulkPlaybackContextResponse.java
@@ -0,0 +1,95 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platforms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2023  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+import com.kaltura.client.utils.request.RequestBuilder;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**
+ * Response object for bulk getPlaybackContext operation.              Each item in
+  the objects array corresponds to the request at the same index.
+ */
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(BulkPlaybackContextResponse.Tokenizer.class)
+public class BulkPlaybackContextResponse extends ListResponse {
+	
+	public interface Tokenizer extends ListResponse.Tokenizer {
+		RequestBuilder.ListTokenizer<BulkResponseObject.Tokenizer> objects();
+	}
+
+	/**
+	 * Array of playback contexts or errors.              Each item corresponds to the
+	  request at the same index in the request array.              Items can be either
+	  KalturaPlaybackContext (success) or KalturaBulkPlaybackContextError (error).
+	 */
+	private List<BulkResponseObject> objects;
+
+	// objects:
+	public List<BulkResponseObject> getObjects(){
+		return this.objects;
+	}
+	public void setObjects(List<BulkResponseObject> objects){
+		this.objects = objects;
+	}
+
+
+	public BulkPlaybackContextResponse() {
+		super();
+	}
+
+	public BulkPlaybackContextResponse(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		objects = GsonParser.parseArray(jsonObject.getAsJsonArray("objects"), BulkResponseObject.class);
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaBulkPlaybackContextResponse");
+		kparams.add("objects", this.objects);
+		return kparams;
+	}
+
+}
+
