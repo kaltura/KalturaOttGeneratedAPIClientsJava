33e75c6bebf9151f88603a5e2c620c46cf56abf0
Backend CI <ott.rnd.core@kaltura.com>
2026-01-23 09:01:07 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1592, branch 12.0.0.2

--- src/main/java/com/kaltura/client/types/ProgramSemanticSearchParams.java
+++ src/main/java/com/kaltura/client/types/ProgramSemanticSearchParams.java
@@ -32,10 +32,6 @@ import com.kaltura.client.Params;
 import com.kaltura.client.types.ObjectBase;
 import com.kaltura.client.utils.GsonParser;
 import com.kaltura.client.utils.request.MultiRequestBuilder;
-import com.kaltura.client.utils.request.RequestBuilder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * This class was generated using exec.php
@@ -45,63 +41,74 @@ import java.util.Map;
  */
 
 /**
- * The configuration object for the metadata enrichment feature.
+ * Program-specific semantic search parameters.              Presence of this
+  object indicates programs should be included in search results.
  */
 @SuppressWarnings("serial")
-@MultiRequestBuilder.Tokenizer(AiMetadataGeneratorConfiguration.Tokenizer.class)
-public class AiMetadataGeneratorConfiguration extends ObjectBase {
+@MultiRequestBuilder.Tokenizer(ProgramSemanticSearchParams.Tokenizer.class)
+public class ProgramSemanticSearchParams extends ObjectBase {
 	
 	public interface Tokenizer extends ObjectBase.Tokenizer {
-		RequestBuilder.MapTokenizer<MetadataFieldConfigurationMap.Tokenizer> assetStructConfigMap();
-		RequestBuilder.ListTokenizer<StringValue.Tokenizer> supportedLanguages();
+		String endsBefore();
+		String expiresAfter();
 	}
 
 	/**
-	 * A type of dictionary defined as [string,KalturaMetadataFieldConfigurationMap].  
-	             This property is used to correlate the newly generated metadata to   
-	            existing metadata IDs which are available in the asset&amp;#39;s
-	  struct with configuration.
+	 * Only include programs that end before this timestamp (Unix epoch seconds).      
+	         Optional filter.
 	 */
-	private Map<String, MetadataFieldConfigurationMap> assetStructConfigMap;
+	private Long endsBefore;
 	/**
-	 * A read only array to list the set of languages which can be used with the
-	  service.              In practice it is populated with the values set in
-	  KalturaMetadataGeneratorLanguages ENUM.
+	 * Only include programs that expire after this timestamp (Unix epoch seconds).    
+	           Optional filter.
 	 */
-	private List<StringValue> supportedLanguages;
+	private Long expiresAfter;
 
-	// assetStructConfigMap:
-	public Map<String, MetadataFieldConfigurationMap> getAssetStructConfigMap(){
-		return this.assetStructConfigMap;
+	// endsBefore:
+	public Long getEndsBefore(){
+		return this.endsBefore;
 	}
-	public void setAssetStructConfigMap(Map<String, MetadataFieldConfigurationMap> assetStructConfigMap){
-		this.assetStructConfigMap = assetStructConfigMap;
-	}
-
-	// supportedLanguages:
-	public List<StringValue> getSupportedLanguages(){
-		return this.supportedLanguages;
+	public void setEndsBefore(Long endsBefore){
+		this.endsBefore = endsBefore;
 	}
 
-	public AiMetadataGeneratorConfiguration() {
+	public void endsBefore(String multirequestToken){
+		setToken("endsBefore", multirequestToken);
+	}
+
+	// expiresAfter:
+	public Long getExpiresAfter(){
+		return this.expiresAfter;
+	}
+	public void setExpiresAfter(Long expiresAfter){
+		this.expiresAfter = expiresAfter;
+	}
+
+	public void expiresAfter(String multirequestToken){
+		setToken("expiresAfter", multirequestToken);
+	}
+
+
+	public ProgramSemanticSearchParams() {
 		super();
 	}
 
-	public AiMetadataGeneratorConfiguration(JsonObject jsonObject) throws APIException {
+	public ProgramSemanticSearchParams(JsonObject jsonObject) throws APIException {
 		super(jsonObject);
 
 		if(jsonObject == null) return;
 
 		// set members values:
-		assetStructConfigMap = GsonParser.parseMap(jsonObject.getAsJsonObject("assetStructConfigMap"), MetadataFieldConfigurationMap.class);
-		supportedLanguages = GsonParser.parseArray(jsonObject.getAsJsonArray("supportedLanguages"), StringValue.class);
+		endsBefore = GsonParser.parseLong(jsonObject.get("endsBefore"));
+		expiresAfter = GsonParser.parseLong(jsonObject.get("expiresAfter"));
 
 	}
 
 	public Params toParams() {
 		Params kparams = super.toParams();
-		kparams.add("objectType", "KalturaAiMetadataGeneratorConfiguration");
-		kparams.add("assetStructConfigMap", this.assetStructConfigMap);
+		kparams.add("objectType", "KalturaProgramSemanticSearchParams");
+		kparams.add("endsBefore", this.endsBefore);
+		kparams.add("expiresAfter", this.expiresAfter);
 		return kparams;
 	}
 
