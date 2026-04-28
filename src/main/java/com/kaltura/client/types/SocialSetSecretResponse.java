776cbcfd1b9f4ced5728864ebbb8efb71524c9e9
Backend CI <ott.rnd.core@kaltura.com>
2026-04-28 18:10:18 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1612, branch 12.2.0.3

--- src/main/java/com/kaltura/client/types/SocialSetSecretResponse.java
+++ src/main/java/com/kaltura/client/types/SocialSetSecretResponse.java
@@ -32,10 +32,6 @@ import com.kaltura.client.Params;
 import com.kaltura.client.types.ObjectBase;
 import com.kaltura.client.utils.GsonParser;
 import com.kaltura.client.utils.request.MultiRequestBuilder;
-import com.kaltura.client.utils.request.RequestBuilder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * This class was generated using exec.php
@@ -45,63 +41,43 @@ import java.util.Map;
  */
 
 /**
- * The configuration object for the metadata enrichment feature.
+ * Set Social IDP Secret Response
  */
 @SuppressWarnings("serial")
-@MultiRequestBuilder.Tokenizer(AiMetadataGeneratorConfiguration.Tokenizer.class)
-public class AiMetadataGeneratorConfiguration extends ObjectBase {
+@MultiRequestBuilder.Tokenizer(SocialSetSecretResponse.Tokenizer.class)
+public class SocialSetSecretResponse extends ObjectBase {
 	
 	public interface Tokenizer extends ObjectBase.Tokenizer {
-		RequestBuilder.MapTokenizer<MetadataFieldConfigurationMap.Tokenizer> assetStructConfigMap();
-		RequestBuilder.ListTokenizer<StringValue.Tokenizer> supportedLanguages();
+		String secretSet();
 	}
 
 	/**
-	 * A type of dictionary defined as [string,KalturaMetadataFieldConfigurationMap].  
-	             This property is used to correlate the newly generated metadata to   
-	            existing metadata IDs which are available in the asset&amp;#39;s
-	  struct with configuration.
+	 * Indicates whether the secret was set.
 	 */
-	private Map<String, MetadataFieldConfigurationMap> assetStructConfigMap;
-	/**
-	 * A read only array to list the set of languages which can be used with the
-	  service.              In practice it is populated with the values set in
-	  KalturaMetadataGeneratorLanguages ENUM.
-	 */
-	private List<StringValue> supportedLanguages;
+	private Boolean secretSet;
 
-	// assetStructConfigMap:
-	public Map<String, MetadataFieldConfigurationMap> getAssetStructConfigMap(){
-		return this.assetStructConfigMap;
-	}
-	public void setAssetStructConfigMap(Map<String, MetadataFieldConfigurationMap> assetStructConfigMap){
-		this.assetStructConfigMap = assetStructConfigMap;
+	// secretSet:
+	public Boolean getSecretSet(){
+		return this.secretSet;
 	}
 
-	// supportedLanguages:
-	public List<StringValue> getSupportedLanguages(){
-		return this.supportedLanguages;
-	}
-
-	public AiMetadataGeneratorConfiguration() {
+	public SocialSetSecretResponse() {
 		super();
 	}
 
-	public AiMetadataGeneratorConfiguration(JsonObject jsonObject) throws APIException {
+	public SocialSetSecretResponse(JsonObject jsonObject) throws APIException {
 		super(jsonObject);
 
 		if(jsonObject == null) return;
 
 		// set members values:
-		assetStructConfigMap = GsonParser.parseMap(jsonObject.getAsJsonObject("assetStructConfigMap"), MetadataFieldConfigurationMap.class);
-		supportedLanguages = GsonParser.parseArray(jsonObject.getAsJsonArray("supportedLanguages"), StringValue.class);
+		secretSet = GsonParser.parseBoolean(jsonObject.get("secretSet"));
 
 	}
 
 	public Params toParams() {
 		Params kparams = super.toParams();
-		kparams.add("objectType", "KalturaAiMetadataGeneratorConfiguration");
-		kparams.add("assetStructConfigMap", this.assetStructConfigMap);
+		kparams.add("objectType", "KalturaSocialSetSecretResponse");
 		return kparams;
 	}
 
