e93d7c12d58366e4cc09d5e5d7b18c8acb9e9ec5
Backend CI <ott.rnd.core@kaltura.com>
2026-07-21 12:38:07 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1635, branch 12.6.0.3

--- src/main/java/com/kaltura/client/types/TextMetaConstraint.java
+++ src/main/java/com/kaltura/client/types/TextMetaConstraint.java
@@ -29,7 +29,7 @@ package com.kaltura.client.types;
 
 import com.google.gson.JsonObject;
 import com.kaltura.client.Params;
-import com.kaltura.client.types.ObjectBase;
+import com.kaltura.client.utils.GsonParser;
 import com.kaltura.client.utils.request.MultiRequestBuilder;
 
 /**
@@ -40,28 +40,71 @@ import com.kaltura.client.utils.request.MultiRequestBuilder;
  */
 
 /**
- * Base class that defines a segment condition.
+ * Filters assets based on a text metadata field containing a substring.
  */
 @SuppressWarnings("serial")
-@MultiRequestBuilder.Tokenizer(BaseSegmentCondition.Tokenizer.class)
-public class BaseSegmentCondition extends ObjectBase {
+@MultiRequestBuilder.Tokenizer(TextMetaConstraint.Tokenizer.class)
+public class TextMetaConstraint extends BaseAttributeConstraint {
 	
-	public interface Tokenizer extends ObjectBase.Tokenizer {
+	public interface Tokenizer extends BaseAttributeConstraint.Tokenizer {
+		String contains();
+		String equals();
+	}
+
+	/**
+	 * The substring that the metadata field value must contain.
+	 */
+	private String contains;
+	/**
+	 * The exact string value the field must equal.
+	 */
+	private String equals;
+
+	// contains:
+	public String getContains(){
+		return this.contains;
+	}
+	public void setContains(String contains){
+		this.contains = contains;
+	}
+
+	public void contains(String multirequestToken){
+		setToken("contains", multirequestToken);
+	}
+
+	// equals:
+	public String getEquals(){
+		return this.equals;
+	}
+	public void setEquals(String equals){
+		this.equals = equals;
+	}
+
+	public void equals(String multirequestToken){
+		setToken("equals", multirequestToken);
 	}
 
 
-
-	public BaseSegmentCondition() {
+	public TextMetaConstraint() {
 		super();
 	}
 
-	public BaseSegmentCondition(JsonObject jsonObject) throws APIException {
+	public TextMetaConstraint(JsonObject jsonObject) throws APIException {
 		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		contains = GsonParser.parseString(jsonObject.get("contains"));
+		equals = GsonParser.parseString(jsonObject.get("equals"));
+
 	}
 
 	public Params toParams() {
 		Params kparams = super.toParams();
-		kparams.add("objectType", "KalturaBaseSegmentCondition");
+		kparams.add("objectType", "KalturaTextMetaConstraint");
+		kparams.add("contains", this.contains);
+		kparams.add("equals", this.equals);
 		return kparams;
 	}
 
--- src/main/java/com/kaltura/client/types/ViewTimeConstraint.java
+++ src/main/java/com/kaltura/client/types/ViewTimeConstraint.java
@@ -30,6 +30,7 @@ package com.kaltura.client.types;
 import com.google.gson.JsonObject;
 import com.kaltura.client.Params;
 import com.kaltura.client.types.ObjectBase;
+import com.kaltura.client.utils.GsonParser;
 import com.kaltura.client.utils.request.MultiRequestBuilder;
 
 /**
@@ -40,28 +41,71 @@ import com.kaltura.client.utils.request.MultiRequestBuilder;
  */
 
 /**
- * Base class that defines a segment condition.
+ * Filters watch actions that occurred within a specific time window.
  */
 @SuppressWarnings("serial")
-@MultiRequestBuilder.Tokenizer(BaseSegmentCondition.Tokenizer.class)
-public class BaseSegmentCondition extends ObjectBase {
+@MultiRequestBuilder.Tokenizer(ViewTimeConstraint.Tokenizer.class)
+public class ViewTimeConstraint extends ObjectBase {
 	
 	public interface Tokenizer extends ObjectBase.Tokenizer {
+		String startTime();
+		String endTime();
+	}
+
+	/**
+	 * The starting time of the viewing window.
+	 */
+	private String startTime;
+	/**
+	 * The ending time of the viewing window.
+	 */
+	private String endTime;
+
+	// startTime:
+	public String getStartTime(){
+		return this.startTime;
+	}
+	public void setStartTime(String startTime){
+		this.startTime = startTime;
+	}
+
+	public void startTime(String multirequestToken){
+		setToken("startTime", multirequestToken);
+	}
+
+	// endTime:
+	public String getEndTime(){
+		return this.endTime;
+	}
+	public void setEndTime(String endTime){
+		this.endTime = endTime;
+	}
+
+	public void endTime(String multirequestToken){
+		setToken("endTime", multirequestToken);
 	}
 
 
-
-	public BaseSegmentCondition() {
+	public ViewTimeConstraint() {
 		super();
 	}
 
-	public BaseSegmentCondition(JsonObject jsonObject) throws APIException {
+	public ViewTimeConstraint(JsonObject jsonObject) throws APIException {
 		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		startTime = GsonParser.parseString(jsonObject.get("startTime"));
+		endTime = GsonParser.parseString(jsonObject.get("endTime"));
+
 	}
 
 	public Params toParams() {
 		Params kparams = super.toParams();
-		kparams.add("objectType", "KalturaBaseSegmentCondition");
+		kparams.add("objectType", "KalturaViewTimeConstraint");
+		kparams.add("startTime", this.startTime);
+		kparams.add("endTime", this.endTime);
 		return kparams;
 	}
 
